f793c4e2a6ba13d5d116c9ba4e3590fcec98230a
Logan Jarrett <logan4179@hotmail.com>
2024-08-12 12:24:43 -0400

    Update LAMS_Utilities.cs
    added better comments for interface methods for explanation to the user

--- LAMS_Utilities.cs
+++ LAMS_Utilities.cs
@@ -11,8 +11,18 @@ namespace LogansAreaManagementSystem
 
     public interface I_LAMS_Entity
     {
+        /// <summary>
+        /// This method allows you to call any logic needed for when an entity becomes activated via an area. This is useful for 
+        /// things like adding an entity to a manager's list of active entities once it's activated, for example. You can just  
+        /// leave this method blank if nothing like this is needed, and it won't hurt the functioning of this system.
+        /// </summary>
         void ActivateMeViaArea();
 
+		/// <summary>
+		/// This method allows you to call any logic needed for when an entity becomes deactivated via an area. This is useful for 
+		/// things like removing an entity from a manager's list of active entities once it's area is deactivated, for example. You 
+        /// can just leave this method blank if nothing like this is needed, and it won't hurt the functioning of this system.
+		/// </summary>
 		void DeactivateMeViaArea();
 
 	}
